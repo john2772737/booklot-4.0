812d473b792200cafb47524cdb05087176f20e70
john <johnregulacion5555@gmail.com>
2024-04-16 07:12:24 -0700

    d

--- admin/src/index.js
+++ admin/src/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import Main from './pages/main';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom'; 
+import App from './app';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
-    <Main />
+    <BrowserRouter> 
+      <App/>
+    </BrowserRouter>
   </React.StrictMode>
 );
-
