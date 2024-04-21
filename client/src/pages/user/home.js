0ea29a932903062581ccbeca8f9376075f3d9e64
JADA <jaduhlgn@gmail.com>
2024-04-22 07:01:29 +0800

    footer footer

---
+++ client/src/pages/user/home.js
@@ -0,0 +1,54 @@
+import React from "react";
+
+function home() {
+  return (
+    <div>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed accumsan,
+        lorem ac varius rutrum, nulla sapien eleifend turpis, ac feugiat sapien
+        magna eget justo. In et tellus ut ipsum suscipit aliquam. Sed sed nisi
+        auctor, tempor metus eu, posuere nisi. Duis at feugiat eros. Nulla ut
+        lectus libero. Morbi elementum consequat turpis, sit amet finibus justo
+        bibendum eget. Sed eget ex a nisl sollicitudin suscipit. Integer
+        sollicitudin felis a odio venenatis, vel luctus magna blandit. Sed
+        dignissim, magna in aliquam pulvinar, ligula elit tempus ipsum, nec
+        ultricies justo purus non sem. Donec rutrum
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed accumsan,
+        lorem ac varius rutrum, nulla sapien eleifend turpis, ac feugiat sapien
+        magna eget justo. In et tellus ut ipsum suscipit aliquam. Sed sed nisi
+        auctor, tempor metus eu, posuere nisi. Duis at feugiat eros. Nulla ut
+        lectus libero. Morbi elementum consequat turpis, sit amet finibus justo
+        bibendum eget. Sed eget ex a nisl sollicitudin suscipit. Integer
+        sollicitudin felis a odio venenatis, vel luctus magna blandit. Sed
+        dignissim, magna in aliquam pulvinar, ligula elit tempus ipsum, nec
+        ultricies justo purus non sem. Donec rutrum
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed accumsan,
+        lorem ac varius rutrum, nulla sapien eleifend turpis, ac feugiat sapien
+        magna eget justo. In et tellus ut ipsum suscipit aliquam. Sed sed nisi
+        auctor, tempor metus eu, posuere nisi. Duis at feugiat eros. Nulla ut
+        lectus libero. Morbi elementum consequat turpis, sit amet finibus justo
+        bibendum eget. Sed eget ex a nisl sollicitudin suscipit. Integer
+        sollicitudin felis a odio venenatis, vel luctus magna blandit. Sed
+        dignissim, magna in aliquam pulvinar, ligula elit tempus ipsum, nec
+        ultricies justo purus non sem. Donec rutrum
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed accumsan,
+        lorem ac varius rutrum, nulla sapien eleifend turpis, ac feugiat sapien
+        magna eget justo. In et tellus ut ipsum suscipit aliquam. Sed sed nisi
+        auctor, tempor metus eu, posuere nisi. Duis at feugiat eros. Nulla ut
+        lectus libero. Morbi elementum consequat turpis, sit amet finibus justo
+        bibendum eget. Sed eget ex a nisl sollicitudin suscipit. Integer
+        sollicitudin felis a odio venenatis, vel luctus magna blandit. Sed
+        dignissim, magna in aliquam pulvinar, ligula elit tempus ipsum, nec
+        ultricies justo purus non sem. Donec rutrum
+      </p>
+    </div>
+  );
+}
+
+export default home;
