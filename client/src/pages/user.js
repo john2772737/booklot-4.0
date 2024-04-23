c457bef108d11afd4df400cbb471ad4faa3f9595
JADA <jaduhlgn@gmail.com>
2024-04-23 21:32:07 +0800

    footer footer may font na, atbp.

--- client/src/pages/user.js
+++ client/src/pages/user.js
@@ -1,9 +1,11 @@
 import React from "react";
 import { Outlet } from 'react-router-dom'; 
 import Footer from "../components/footer";
+import Navbar from "../components/navbar";
 function user() {
   return (
     <div className="user">
+      <Navbar />
       <Outlet />
       <Footer />
     </div>
